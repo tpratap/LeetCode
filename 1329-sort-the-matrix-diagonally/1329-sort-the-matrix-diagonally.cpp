ce61cbc7641d4a1fbf11b9a94352374a0248f0bc
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-22 13:25:27 +0530

    Time: 11 ms (80.10%), Space: 9.5 MB (18.62%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,60 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        vector<vector<int>> ans;
         int m = mat.size(), n = mat[0].size();
-        
-        //upper triangular matrix
-        for(int k = 0; k < n; k++)  {
-            int j = k,i = 0;
-            vector<int> temp;
-            while(i < m && j < n) {
-                temp.push_back(mat[i][j]);
-                i++;
-                j++;
-            }
-            ans.push_back(temp);
-        }
-        
-        //lower triangular matrix
-        for(int k = 1; k < m; k++)  {
-            int i = k,j = 0;
-            vector<int> temp;
-            while(i < m && j < n) {
-                temp.push_back(mat[i][j]);
-                i++;
-                j++;
-            }
-            ans.push_back(temp);
-        }
-        
-        //Sort Matrix Diagonally
-        for(int i = 0; i < ans.size(); i++) {
-             sort(ans[i].begin(),ans[i].end());
-        }
-        
-        //upper triangular matrix
-        for(int k = 0; k < n; k++)  {
-            int j = k,i = 0;
-            while(i < m && j < n) {
-                mat[i][j] = ans[k][i];
-                i++;
-                j++;
-            }
-            
-        }
-        
-        //lower triangular matrix
-        for(int k = 1, l = n; k < m; k++,l++)  {
-            int i = k,j = 0;
-            while(i < m && j < n) {
-                mat[i][j] = ans[l][j];
-                i++;
-                j++;
+     
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map; 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i - j].push(mat[i][j]);
             }
         }
-        
-        
+     
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = map[i - j].top(); 
+                map[i - j].pop();
+            }
+        }
+     
         return mat;
     }
 };
