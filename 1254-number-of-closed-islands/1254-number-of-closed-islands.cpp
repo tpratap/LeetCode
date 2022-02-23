217b67fe64dfbf5592daae37e29a774d8dbecc5e
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-23 22:42:12 +0530

    Time: 22 ms (39.58%), Space: 9.8 MB (29.95%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
-   
-    int m,n;
-    bool isClosed;
-    void dfs(vector<vector<int>>& grid, int i, int j) {
+    int dir[4][2]={{-1,0},{0,-1},{0,1},{1,0}};
+    bool dfs(vector<vector<int>> &grid,vector<vector<bool>> &visited,int m, int n,int i,int j)
+    {
+        visited[i][j]=1;
+        if(i == 0|| i == m-1 || j==0 || j == n-1)
+            return false;
         
-        if(i < 0 || j < 0 || i >= m || j >= n) 
+        bool ans = true;
+        
+        for(int k=0;k<4;k++)
         {
-            isClosed = false;
-            return;
+            int x = i + dir[k][0];
+            int y = j + dir[k][1];
+            
+            if(x >= 0 && y >= 0 && x < m && y < n && !visited[x][y] && grid[x][y] == 0)
+            {
+                if(!dfs(grid,visited, m, n, x, y))
+                    ans=false;
+            }
         }
-        
-        if(grid[i][j] == 1 || grid[i][j] == -1)
-           return;
-
-       if(grid[i][j] == 0)
-           grid[i][j] = -1;
-        
-        dfs(grid, i+1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
+        return ans;
     }
     
+    
     int closedIsland(vector<vector<int>>& grid) {
-        int island = 0;
-        m = grid.size();
-        n = grid[0].size();
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 0) {
-                    isClosed = true;
-                    dfs(grid, i, j);
-                    if(isClosed) {
-                        island++;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        int closedIslands=0;
+        for(int i = 1;i < m-1 ; i++)
+        {
+            for(int j = 1;j < n-1; j++)
+            {
+                if(!visited[i][j] && grid[i][j]==0)
+                {
+                    if(dfs(grid,visited,m,n,i,j))
+                    {
+                        closedIslands++;
                     }
                 }
             }
         }
-        return island;
+        return closedIslands;
+        
     }
 };
