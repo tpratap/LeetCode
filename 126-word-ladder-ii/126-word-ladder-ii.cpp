a139e5c8612b3d6e8c162ad0bbc4b47e44f3e6c8
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-24 14:43:59 +0530

    Time: 24 ms (56.04%), Space: 12.2 MB (37.97%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,36 +1,49 @@
 class Solution {
+    vector<string> findNeighbors(string word, unordered_set<string>& wordList)  {
+        vector<string> neighbors;
+        for(int i = 0; i < word.size(); ++i){
+            int s = word[i];
+            for(char c = 'a'; c <= 'z'; ++c){
+                word[i] = c;
+                if(wordList.find(word) != wordList.end())
+                    neighbors.push_back(word);
+            }
+            word[i] = s;
+        }
+        return neighbors;
+    }
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st (wordList.begin(),wordList.end());
+        unordered_set<string> word_list(wordList.begin(), wordList.end());
+        vector<vector<string>> result;
+        
+        queue<vector<string>> Q;
+        Q.push({beginWord});
+        bool reached_end = false;
         unordered_set<string> visited;
-        vector<vector<string>> ans;
-        queue<vector<string>> q;
-        q.push({beginWord});
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto curr_path = q.front();
-                q.pop();
-                string last = curr_path.back();
-                for(int i = 0 ; i < last.size() ; ++i){
-                    auto temp = last;
-                    for(char ch = 'a' ; ch <= 'z' ; ++ch){
-                        temp[i] = ch;
-                        if(st.find(temp) != st.end()){
-                            auto new_path = curr_path;
-                            new_path.push_back(temp);
-                            visited.insert(temp);
-                            if(temp == endWord)
-                                ans.push_back(new_path);
-                            else
-                                q.push(new_path);
-                        }
+        
+        while(!Q.empty()){
+            int n = Q.size();
+            for(int i = 0; i < n; ++i){
+                vector<string> path = Q.front(); // running path 
+                Q.pop();
+                vector<string> neighbors = findNeighbors(path.back(), word_list);
+                for(int j = 0; j < neighbors.size(); ++j){
+                    vector<string> new_path(path.begin(), path.end());
+                    new_path.push_back(neighbors[j]);
+                    visited.insert(neighbors[j]);
+                    Q.push(new_path);
+                    if(neighbors[j] == endWord){
+                        result.push_back(new_path);
+                        reached_end = true;
                     }
                 }
             }
-            for(auto& str : visited)
-                    st.erase(str);
+            if(reached_end) break;
+            for(auto it = visited.begin(); it != visited.end(); ++it)
+                word_list.erase(*it);
+            visited.clear();
         }
-        return ans;
+        return result;
     }
 };
