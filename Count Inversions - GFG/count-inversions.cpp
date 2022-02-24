aac04505bdb1a00d8af1b9c9dbd5e64f0ba684e7
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-24 17:32:16 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -6,123 +6,98 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
   public:
-    // arr[]: Input Array
-    // N : Size of the Array arr[]
-    // Function to count inversions in the array.
-    // long long merge(long long arr[], long long temp[], int left, int mid, int right) {
-    //     int i, j, k;
-    //     i = left;
-    //     j = mid;
-    //     k = right;
-    //     long long inv_count = 0;
-    //     while(i <= mid-1 && j <= right) {
-    //         if(arr[i] <= arr[j]) {
-    //             temp[k] = arr[i];
-    //             k++;
-    //             i++;
-    //         }
-    //         else {
-    //             temp[k] = arr[j];
-    //             k++;
-    //             j++;
-    //             inv_count = inv_count + (long long)(mid-i);
-    //         }
-    //     }
+    long long merge(long long arr[], long long temp[], int left, int mid, int right) {
+        int i, j, k;
+        i = left;
+        j = mid;
+        k = left;
+        long long inv_count = 0;
+        while(i <= mid-1 && j <= right) {
+            if(arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                k++;
+                i++;
+            }
+            else {
+                temp[k] = arr[j];
+                k++;
+                j++;
+                inv_count = inv_count + (long long)(mid-i);
+            }
+        }
         
-    //     while(i  <= mid -1) {
-    //         temp[k] = arr[i];
-    //         k++;
-    //         i++;
-    //     }
+        while(i  <= mid -1) {
+            temp[k] = arr[i];
+            k++;
+            i++;
+        }
         
-    //     while(j <= right) {
-    //         temp[k] = arr[j];
-    //         k++;
-    //         j++;
-    //     }
+        while(j <= right) {
+            temp[k] = arr[j];
+            k++;
+            j++;
+        }
         
-    //     for(i = left; i <= right; i++)
-    //         arr[i] = temp[i];
+        for(i = left; i <= right; i++)
+            arr[i] = temp[i];
             
-    //     return inv_count;
-    // }
-    // long long int mergeSort(long long arr[], long long temp[], int low, int high) {
-    //     long long inv_count = 0;
-    //     if(low < high) {
-    //     int mid = (low + high)/2;
-    //         inv_count += mergeSort(arr,temp, low, mid);
-    //         inv_count += mergeSort(arr, temp, mid+1, high);
-    //         inv_count += merge(arr, temp,low, mid+1, high);
-    //     }
-    //     return inv_count;
-    // }
+        return inv_count;
+    }
+    
 
 
 
-/* This funt merges two sorted arrays
-and returns inversion count in the arrays.*/
-long long merge(long long arr[], long long temp[], int left, int mid,
-		int right)
-{
-	int i, j, k;
-	long long inv_count = 0;
+// /* This funt merges two sorted arrays
+// and returns inversion count in the arrays.*/
+// long long merge(long long arr[], long long temp[], int left, int mid,
+// 		int right)
+// {
+// 	int i, j, k;
+// 	long long inv_count = 0;
 
-	i = left; /* i is index for left subarray*/
-	j = mid; /* j is index for right subarray*/
-	k = left; /* k is index for resultant merged subarray*/
-	while ((i <= mid - 1) && (j <= right)) {
-		if (arr[i] <= arr[j]) {
-			temp[k++] = arr[i++];
-		}
-		else {
-			temp[k++] = arr[j++];
+// 	i = left; /* i is index for left subarray*/
+// 	j = mid; /* j is index for right subarray*/
+// 	k = left; /* k is index for resultant merged subarray*/
+// 	while ((i <= mid - 1) && (j <= right)) {
+// 		if (arr[i] <= arr[j]) {
+// 			temp[k++] = arr[i++];
+// 		}
+// 		else {
+// 			temp[k++] = arr[j++];
 
-			/* this is tricky -- see above
-			explanation/diagram for merge()*/
-			inv_count = inv_count + (mid - i);
-		}
-	}
+// 			/* this is tricky -- see above
+// 			explanation/diagram for merge()*/
+// 			inv_count = inv_count + (mid - i);
+// 		}
+// 	}
 
-	/* Copy the remaining elements of left subarray
-(if there are any) to temp*/
-	while (i <= mid - 1)
-		temp[k++] = arr[i++];
+// 	/* Copy the remaining elements of left subarray
+// (if there are any) to temp*/
+// 	while (i <= mid - 1)
+// 		temp[k++] = arr[i++];
 
-	/* Copy the remaining elements of right subarray
-	(if there are any) to temp*/
-	while (j <= right)
-		temp[k++] = arr[j++];
+// 	/* Copy the remaining elements of right subarray
+// 	(if there are any) to temp*/
+// 	while (j <= right)
+// 		temp[k++] = arr[j++];
 
-	/*Copy back the merged elements to original array*/
-	for (i = left; i <= right; i++)
-		arr[i] = temp[i];
+// 	/*Copy back the merged elements to original array*/
+// 	for (i = left; i <= right; i++)
+// 		arr[i] = temp[i];
 
-	return inv_count;
-}
-    /* An auxiliary recursive function
-that sorts the input array and
-returns the number of inversions in the array. */
-long long _mergeSort(long long arr[], long long temp[], int left, int right)
-{
-	int mid;
-	long long inv_count = 0;
-	if (right > left) {
-		/* Divide the array into two parts and
-		call _mergeSortAndCountInv()
-		for each of the parts */
-		mid = (right + left) / 2;
-
-		/* Inversion count will be sum of
-		inversions in left-part, right-part
-		and number of inversions in merging */
-		inv_count += _mergeSort(arr, temp, left, mid);
-		inv_count += _mergeSort(arr, temp, mid + 1, right);
-
-		/*Merge the two parts*/
-		inv_count += merge(arr, temp, left, mid + 1, right);
-	}
-	return inv_count;
-}
+// 	return inv_count;
+// }
+   
+long long int _mergeSort(long long arr[], long long temp[], int low, int high) {
+        long long inv_count = 0;
+        if(low < high) {
+        int mid = (low + high)/2;
+            inv_count += _mergeSort(arr,temp, low, mid);
+            inv_count += _mergeSort(arr, temp, mid+1, high);
+            inv_count += merge(arr, temp,low, mid+1, high);
+        }
+        return inv_count;
+    }
 
     long long int inversionCount(long long arr[], long long N)
     {
