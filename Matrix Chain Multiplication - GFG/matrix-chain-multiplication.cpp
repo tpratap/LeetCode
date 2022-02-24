2d7e3a017c4c2e89520655e46b58ce392a1851f8
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-24 23:38:41 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,27 +9,44 @@ using namespace std;
 
 class Solution{
 public:
-int dp[101][101];
-    int MCM(int arr[], int i, int j) {
-        if(i+1 == j)
-            return dp[i][j] = 0;
+    // int dp[101][101];
+    // int MCM(int arr[], int i, int j) {
+    //     if(i+1 == j)
+    //         return dp[i][j] = 0;
         
             
-        int res = INT_MAX;
+    //     int res = INT_MAX;
     
-        if(dp[i][j] != -1)
-            return dp[i][j];
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
             
-        for(int k = i+1; k < j; k++) {
-            res = min(res, MCM(arr,i,k) + MCM(arr, k, j) + arr[i]*arr[k]*arr[j]);
-        }
+    //     for(int k = i+1; k < j; k++) {
+    //         res = min(res, MCM(arr,i,k) + MCM(arr, k, j) + arr[i]*arr[k]*arr[j]);
+    //     }
         
-        return dp[i][j] = res;
-    }
+    //     return dp[i][j] = res;
+    // }
     int matrixMultiplication(int N, int arr[])
     {
-        memset(dp,-1,sizeof(dp));
-        return MCM(arr, 0, N-1);
+        //memset(dp,-1,sizeof(dp));
+        //return MCM(arr, 0, N-1);
+        int dp[N][N];
+        
+        for(int i = 0; i < N-1; i++) {
+            dp[i][i+1] = 0;
+        }
+        
+        for(int gap = 2; gap < N; gap++) {
+            for(int i = 0; i + gap < N; i++) {
+                int j = i + gap;
+                dp[i][j] = INT_MAX;
+                
+                for(int k = i+1; k < j; k++) {
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + arr[i]*arr[k]*arr[j]);
+                }
+            } 
+        }
+        return dp[0][N-1];
     }
 };
 
