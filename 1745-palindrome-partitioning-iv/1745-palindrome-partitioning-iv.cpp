8c462e62b3cc1f9a209ce7ce01953282bb3e1ec8
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-25 11:53:55 +0530

    Time: 447 ms (36.67%), Space: 17.4 MB (67.80%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    bool checkPartitioning(string &s) {
+        int n = s.length();
+        if(n == 3)
+            return true;
+        
+        if(n < 3)
+            return false;
+        
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
+        for(int i = 0; i < n; i++) {
+            isPal[i][i] = true;
+        }
+        
+        for (int L = 2; L <= n; L++) {
+        for (int i = 0; i < n - L + 1; i++) {
+            int j = i + L - 1;
+            if (L == 2)
+                isPal[i][j] = (s[i] == s[j]);
+            else
+                isPal[i][j] = (s[i] == s[j]) && isPal[i + 1][j - 1];
+        }
+    }
+
+        
+        for(int i = 1; i < n-1; i++) {
+            for(int j = i; j < n-1; j++) {
+                if(isPal[0][i-1] && isPal[i][j] && isPal[j+1][n-1]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
