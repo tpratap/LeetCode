868e203042e7420bdefd16caa0d7ddd91f46b7c3
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-25 09:28:07 +0530

    Time: 360 ms (17.44%), Space: 163 MB (16.29%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    bool isPalindrome(string &s, int i, int j) {
-        while(i <= j) {
-            if(s[i] == s[j]) {
-                i++;
-                j--;
-            }
-            else {
-                return false;
-            }
-        }    
-        return true;
-    }
     
-    void palpart(string &s, int i, int j, vector<string> curr) {
+    void palpart(string &s, int i, int j, vector<string> curr, vector<vector<bool>> &isPal) {
         if(i >= j) {
             ans.push_back(curr);
         }
         
         for(int k = i; k < j; k++) {
-            if(isPalindrome(s,i,k)) {
+            if(s[i] == s[k] && ( k - i <= 2 || isPal[i+1][k-1])) {
+                isPal[i][k] = true;
                 curr.push_back(s.substr(i, k - i + 1));
-                palpart(s, k+1, j, curr);
+                palpart(s, k+1, j, curr, isPal);
                 curr.pop_back();
             }
         }
@@ -30,8 +19,9 @@ public:
     
     vector<vector<string>> partition(string &s) {
         int n = s.length();
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
         vector<string> curr;
-        palpart(s, 0 , n, curr);
+        palpart(s, 0 , n, curr, isPal);
         return ans;
     }
 };
