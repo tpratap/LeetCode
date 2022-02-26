a1c0e2216a779dd14edc9f1c94467012adce5060
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-27 01:47:40 +0530

    Time: 15 ms (94.15%), Space: 8.9 MB (87.63%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -1,81 +1,36 @@
 class Solution {
 public:
-    // Class to carry three values at a time
-    class tuple
-    {
-        public:
-        int node; // on which current node we are standing
-        int mask; // mask of that node
-        int cost; // cost of path explore by this node
-        tuple(int node, int mask, int cost)
-        {
-            this -> node = node;
-            this -> mask = mask;
-            this -> cost = cost;
-        }
-    };
-    
-    
     int shortestPathLength(vector<vector<int>>& graph) {
-        // total number of nodes present
-        int n = graph.size(); // extracting size of graph
-        
-        
-        queue<tuple> q; // queue of class tuple type
-        
-         // set to take care which path we have already visited
-        set<pair<int, int>> vis;
-        
-        int all = (1 << n) - 1; // if all nodes visited then
-            
-        // we don't know which node will give us shortest path so intially for all nodes we will store in our queue
-        for(int i = 0; i < n; i++)
-        {
-            int maskValue = (1 << i); // 2 ^ i
-            
-            tuple thisNode(i, maskValue, 1); // make a tuple for every nod
-            
-            q.push(thisNode); // push tuple into our queue
-            
-            vis.insert({i, maskValue}); // also update into our set
+        int n=graph.size();
+        if(n==1) return 0; // if there is only one element than the shorted path to itself is 0
+        int final_mask= (1<<n)-1; // if n=3 the max we can go is 111 or 7
+        queue<pair<int,int>> q; // pair<element,mask>
+         vector<vector<bool>> seen(n,vector<bool>(final_mask+1,false));
+        for(int i=0;i<n;i++){
+		//store <element,mask> we are storing all of them because the initial state is not fixed and we are just interested in shortest path covering all nodes
+            q.push({i,(1<<i)}); 
+            seen[i][1<<i]=true; // as we have visited this node(inserted in queue)
         }
-        
-        // Implementing BFS
-        while(q.empty() == false) // until queue is not empty
-        {
-            tuple curr = q.front(); // extracting front tuple
-            q.pop(); // pop from queuu
-            
-            // if mask value becomes all, that means we have visited all of our nodes, so from here return cost - 1
-            if(curr.mask == all) 
-            {
-                return curr.cost - 1;
-            }
-            
-            // if not, start exploring in its adjcant
-            for(auto &adj: graph[curr.node])
-            {
-                int bothVisitedMask = curr.mask; // current mask
-                
-                // we are moving from one node o anthor node
-                bothVisitedMask = bothVisitedMask | (1 << adj); 
-                
-                // make tuple of this path
-                tuple ThisNode(adj, bothVisitedMask, curr.cost + 1);
-                
-                // if this path is not explored i.e
-                // if it is not present in our set then,
-                if(vis.find({adj, bothVisitedMask}) == vis.end())
-                {
-                    vis.insert({adj, bothVisitedMask}); // insert into set
-                    
-                    q.push(ThisNode); // also insert into queue
-                }
-            }
-            
-        }
-        
-        // return -1, but this condition will never come
-        return -1;
+        int ans=0;
+		//traditional BFS
+        while(!q.empty()){
+            int sz=q.size();
+            ans++;
+            while(sz--){
+                pair<int,int> pq=q.front();
+                q.pop();
+                int element=pq.first;
+                int curr_mask=pq.second;
+                for(int& child:graph[element]){
+				//creating a new mask using new element and old mask, ex- if we have visited 1st and 2nd and now its our third element then 011 | 100=111 
+                     int new_mask=(1<<child) | curr_mask;  
+                    if(new_mask==final_mask) return ans; 
+                    if(!seen[child][new_mask]){ //important , this way we are stopping infinite loops
+                        q.push({child,new_mask});
+                       seen[child][new_mask]=true;
+                    }
+                }     
+            } 
+        }     return -1;
     }
 };
