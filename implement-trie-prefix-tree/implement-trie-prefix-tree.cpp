501980df743c72dc85a2863ebe8597d7bd0cfbe8
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-02-28 19:22:21 +0530

    Time: 94 ms, Memory: 44.9 MB - LeetHub

---
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,69 @@
+class Node {
+public:
+    Node* links[26];    
+    bool flag = false;
+    
+    bool containsKey(char ch) {
+        return (links[ch-'a'] != NULL);
+    }
+    
+    void put(char ch, Node* node) {
+        links[ch-'a'] = node; 
+    }
+    
+    Node* get(char ch) {
+        return links[ch-'a']; 
+    }
+    
+    void setEnd() {
+        flag = true; 
+    }
+    
+    bool isEnd() {
+        return flag; 
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node *node = root;
+        for(int i = 0;i<word.size();i++) {
+            if(!node->containsKey(word[i])) {
+                node->put(word[i], new Node()); 
+            }
+            node = node->get(word[i]); 
+        }
+        node->setEnd(); 
+    }
+    
+    bool search(string word) {
+        Node *node = root; 
+        for(int i = 0;i<word.size();i++) {
+            if(!node->containsKey(word[i])) {
+                return false; 
+            }
+            node = node->get(word[i]); 
+        }
+        if(node->isEnd()) {
+            return true; 
+        }
+        return false; 
+    }
+    
+    bool startsWith(string prefix) {
+        Node *node = root; 
+        for(int i = 0;i<prefix.size();i++) {
+            if(!node->containsKey(prefix[i])) {
+                return false; 
+            }
+            node = node->get(prefix[i]); 
+        }
+        return true; 
+    }
+};
