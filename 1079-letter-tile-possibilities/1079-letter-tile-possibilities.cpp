bc9e88d3c192f4181b5987873774c028b45ed63c
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-01 23:00:56 +0530

    Time: 144 ms (23.56%), Space: 26.6 MB (7.10%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,22 +1,21 @@
 class Solution {
+unordered_set<string> s;
 public:
-    
-    void dfs(string &tiles, unordered_set<string> &s, vector<int> &visited, string curString) {
-        if(curString != "") s.insert(curString);
-        
-        for(int i = 0; i < tiles.size(); i++) {
-            if(visited[i]) continue;
-            
-            visited[i] = 1;
-            dfs(tiles, s, visited, curString + tiles[i]);
-            visited[i] = 0; // backtrack
-        }
+    void dfs(string &tiles, string curr, vector<int> visited) {
+        if(curr != "") s.insert(curr);
+        for(int j = 0; j < tiles.size(); ++j) {
+            if(visited[j])
+                continue;
+            visited[j] = 1;
+            curr.push_back(tiles[j]);
+            dfs(tiles, curr, visited);
+            visited[j] = 0;
+            curr.pop_back();
+        } 
     }
-    
-    int numTilePossibilities(string tiles) {
-        unordered_set<string> s;
+    int numTilePossibilities(string &tiles) {
         vector<int> visited(tiles.size());
-        dfs(tiles, s, visited, "");
+        dfs(tiles, "", visited);
         return s.size();
     }
 };
