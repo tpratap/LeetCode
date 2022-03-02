871d64377d39a2b74b8b3a5a24ce430e00190581
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-02 21:39:08 +0530

    Time: 82 ms (69.96%), Space: 48.4 MB (29.15%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int dp[100001];
-    Solution() {
-        memset(dp, -1, sizeof(dp));
-    }
+    // int dp[100001];
+    // Solution() {
+    //     memset(dp, -1, sizeof(dp));
+    // }
     
-    bool canJump(vector<int>& nums, int i = 0) {
-        if(i == nums.size()-1)
-        return dp[i] = true;
+    bool canJump(vector<int>& nums) {
+//         if(i == nums.size()-1)
+//         return dp[i] = true;
     
-        if(i > nums.size()-1) 
-            return false;
+//         if(i > nums.size()-1) 
+//             return false;
 
-        if(dp[i]!=-1) 
-            return dp[i];
+//         if(dp[i]!=-1) 
+//             return dp[i];
 
-        for(int j=1;j<=nums[i];j++) {
-            if(canJump(nums,i+j)) 
-                return dp[i] = true;
+//         for(int j=1;j<=nums[i];j++) {
+//             if(canJump(nums,i+j)) 
+//                 return dp[i] = true;
+//         }
+
+//         return dp[i] = false;
+        //[2 3 1 1 4]  = 4th
+        //[3 2 1 0 4] = [3rd]
+        //[2,0,6,9,8,4,5,0,8] [8th]
+        int n = nums.size();
+        int maxJump = 0;
+        for(int i = 0; i < n; i++) {
+            if(maxJump < i)
+                return false;
+            maxJump = max(maxJump, i + nums[i]);
         }
-
-        return dp[i] = false;
+        
+        return true;
     }
 };
