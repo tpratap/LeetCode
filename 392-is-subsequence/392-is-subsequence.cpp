a5aa3c9a403841f048cfb7d93f0cd4055088369c
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-02 19:36:37 +0530

    Time: 150 ms (5.04%), Space: 288.7 MB (5.07%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        int i = 0, j = 0;
-        while(i < m && j < n) {
-            if(s[i] == t[j]) {
-                i++;
-            }
-            j++;
+    bool recur(string s, string t, int m, int n, vector<vector<bool>>& dp) {
+        if(m == 0)
+            return dp[m][n] = true;
+        if(n == 0)
+            return dp[m][n] = false;
+        if(m > n)
+            return dp[m][n] = false;
+        
+        if(dp[m][n] != false)
+            return true;
+        
+        if(s[m-1] == t[n-1]) {
+            return dp[m][n] = recur(s,t,m-1,n-1, dp);
         }
-        return i == m ? 1 : 0;
+        return dp[m][n] =  recur(s,t,m,n-1, dp);
+    }
+    bool isSubsequence(string s, string t) {
+        int m = s.length(), n = t.length();
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        return recur(s,t,m,n, dp);
     }
 };
