57e5ee34f0056c389755369d0efbc9149180b574
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-03 18:41:23 +0530

    Time: 4 ms (71.13%), Space: 8.2 MB (81.05%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -4,14 +4,14 @@ public:
         int res = 0;
         int n = arr.size();
         for(int i = 0; i < n; ++i) {
-            int start = n - i;
-            int end = i + 1;
-            int total = start*end;
-            int odd = total/2;
+            int start = n - i; //subarrays starting at index i
+            int end = i + 1;   // subarrays ending at index i
+            int total = start*end; //Total subarrays that contains i
+            int odd = total/2; // odd length subarrays that contains i
             if(total%2 == 1) {
-                odd++;
+                odd++; // Odd = even or odd = even + 1
             }
-            res += odd*arr[i];
+            res += odd*arr[i]; // Sum contributed by arr[i] is product of its occurance and its value 
         }
         return res;
     }
