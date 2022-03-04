5bea282550060aeadda2e7f4ff14677d6c033dbe
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-04 20:15:19 +0530

    Time: 71 ms (68.38%), Space: 21.8 MB (59.88%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -24,21 +24,21 @@ public:
         int m = grid[0].size();
         
         for(int j = 0;j<m;j++){
-            if(grid[0][j])
-            dfs(grid,0,j,n,m);
-             if(grid[n-1][j])
-            dfs(grid,n-1,j,n,m);
+            if(grid[0][j] == 1)
+                dfs(grid,0,j,n,m);
+            if(grid[n-1][j] == 1)
+                dfs(grid,n-1,j,n,m);
         }
          for(int i = 0;i<n;i++){
-            if(grid[i][0])
-            dfs(grid,i,0,n,m);
-            if(grid[i][m-1])
-            dfs(grid,i,m-1,n,m);
+            if(grid[i][0] == 1)
+                dfs(grid,i,0,n,m);
+            if(grid[i][m-1] == 1)
+                dfs(grid,i,m-1,n,m);
         }
         int count = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j])
+                if(grid[i][j] == 1)
                    count++;
             }
         }
