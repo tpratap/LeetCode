865e6a5f998833342b355138fea160f63eadc539
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-06 14:26:35 +0530

    Time: 290 ms (35.00%), Space: 65.8 MB (50.00%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -1,25 +1,28 @@
-typedef long long LL;
 class Solution {
 public:
-    long long minimalKSum(vector<int>& nums, int k) {
-        LL sum = (1LL + k) * k / 2;
-        LL cnt = 0;
-        unordered_set<LL> Set(nums.begin(), nums.end());
-        for (auto i : Set) {
-            if (i >= 1 && i <= (1 + k - 1)) {
-                sum -= i;
-                cnt++;
+    long long minimalKSum(vector<int>& nums, int k){
+        sort(nums.begin(), nums.end());
+        long long s = 0;
+        int i = 0;
+        int prev = 0;
+        while(k>0 && i<nums.size()){
+            int len = nums[i] - prev - 1;
+            if(len>k) len = k;
+            if(len>0){
+                long long int start = prev;
+                long long int end = start + len;
+                long long int sum = (end*(end+1))/2 - (start*(start+1))/2;
+                s += sum;
+                k -= len;
             }
+            prev = nums[i]; i++;
         }
-        
-        LL target = k + 1LL;
-        while (cnt--) {
-            while (Set.find(target) != Set.end()) {
-                target++;
-            }
-            sum += target;
-            target++;
+        if(k>0){
+            long long int start = prev;
+            long long int end = start + k;
+            long long int sum = (end*(end+1))/2 - (start*(start+1))/2;
+            s += sum;
         }
-        return sum;
+        return s;
     }
 };
