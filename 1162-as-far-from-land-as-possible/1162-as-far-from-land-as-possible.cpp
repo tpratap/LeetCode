e641c3399edca3ed57494027e02da14741394641
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-10 18:35:25 +0530

    Time: 101 ms (44.10%), Space: 21.4 MB (33.09%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        
-        int dir[4][2] = {{-1, 0},{0, -1},{0, 1},{1, 0}}; 
-        
+        int dir[4][2] = {{-1, 0},{0, -1},{0, 1},{1, 0}};
         int n = grid.size();
         
-        vector<vector<int>> dist(n, vector<int>(n , -1));
+        vector<vector<int>> dist(n, vector<int>(n , 0));
         
         queue<pair<int,int>> q;
         
@@ -15,16 +13,16 @@ public:
         for(int i=0; i < n; ++i){
             for(int j =0 ; j < n; ++j){
                 if(grid[i][j] == 1){
-                    dist[i][j] = 0;
-                    zero = false; //If all the cells are ones, it will remain true
+                    zero = false;
                     q.push({i, j});  
-                } else{
-                    ones = false; //If all the cells are ones, it will remain true 
+                }else{
+                    ones = false;
+                    dist[i][j] = INT_MAX;
                 }
             }
         }
         
-        if(zero || ones) // If all cells only contains 0 or 1 then return -1 
+        if(zero || ones)
             return -1;
         
         while(!q.empty()){
@@ -34,9 +32,9 @@ public:
             for(int k =0 ; k < 4; ++k){
                 int x = i + dir[k][0];
                 int y = j + dir[k][1];
-                if(x >= 0 && y >= 0 && x < n && y < n) { //Valid Adjacent
-                    if(dist[x][y] == -1) { 
-                        if(grid[x][y] == 0) {        //Distance from 1 to 1 is always 0, so upadate 0 to 1 distance only
+                if(x >= 0 && y >= 0 && x < n && y < n) {
+                    if(dist[i][j] + 1 < dist[x][y]) {
+                        if(grid[x][y] == 0){
                             dist[x][y] = 1 + dist[i][j];
                             q.push({x, y});
                         }
@@ -49,7 +47,7 @@ public:
         for(int i = 0; i <n; ++i){
             for(int j=0; j <n; ++j){
                 if(grid[i][j] == 0)
-                    res = max(res, dist[i][j]); //Find out the maximum distance
+                    res = max(res, dist[i][j]);
             }
         }
         return res;
