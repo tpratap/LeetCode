b560f12c5dacc968d47bc973c697ae36bbaef542
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-12 22:31:46 +0530

    Time: 8 ms (64.30%), Space: 8.5 MB (90.04%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-//     int solve(vector<vector<int>>& triangle, int i, int j) {
-            
-//     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        
+        vector<int> dp(n, INT_MAX);
+        dp[0] = triangle[0][0];
         for(int i = 1; i < n; ++i) {
+            int val = 0;
             for(int j = 0; j <= i; ++j) {
-                if(j == 0)
-                    triangle[i][j] += triangle[i-1][j];
-                else if(j == i)
-                    triangle[i][j] += triangle[i-1][j-1];
-                else
-                    triangle[i][j] += min(triangle[i-1][j] ,triangle[i-1][j-1]);
+                int newval;
+                if(j == 0) {
+                    newval = dp[j];
+                    dp[j] = triangle[i][j] + dp[j];
+                }
+                else if(j == i) {
+                    //newval = dp[j];
+                    dp[j] = triangle[i][j] + val;
+                }
+                else {
+                    newval = dp[j];
+                    dp[j] = triangle[i][j] + min(dp[j] , val);
+                }
+                val = newval;
             }
         }
         int ans = INT_MAX;
         for(int i = 0; i < n; ++i)
-            ans = min(ans, triangle[n-1][i]);
+            ans = min(ans, dp[i]);
         
         return ans;
     }
