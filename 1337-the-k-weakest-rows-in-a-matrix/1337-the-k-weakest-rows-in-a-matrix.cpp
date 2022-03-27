fac43051fc42b28c8de3bea495ffaf48130176cd
Tej Pratap <59302308+tpratap@users.noreply.github.com>
2022-03-27 14:20:40 +0530

    Time: 22 ms (27.63%), Space: 10.6 MB (42.57%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    int findLastOne(vector<int>& arr, int n) {
+        int low = 0, high = n-1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == 0 && (mid == 0 || arr[mid - 1] == 1))
+                return mid;
+            else if (arr[mid] == 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return n;
+    }
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pair<int, int>> pq;
+        int m = mat.size();
+        int n = mat[0].size();
+        for(int i = 0; i < m; ++i) {
+            int count = findLastOne(mat[i], n);
+            pq.push({count, i});
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        vector<int> ans;
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
